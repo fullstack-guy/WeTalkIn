0e1ccd5898dd4eefd41c1fbee48ff4b1a939f19f
fullstack-guy <andriihryhoriev1@gmail.com>
2022-12-30 11:36:18 -0600

    update file

--- src/App.js
+++ src/App.js
@@ -26,12 +26,12 @@ function App () {
 
                 <h1>WeTalkin.com</h1>
 
-                
+
                 <p className='bodyHeading'>
                     Download on the AppStore:
                 </p>
 
-                <a class="projectLink" href="https://wetalkin.com"><button type="button"
+                <a class="projectLink" href="https://apps.apple.com/us/app/wetalkin/id1598817389"><button type="button"
                     class="btn btn-outline-success btn-warning ml-2 mr-2 mt-0 mb-4">WeTalkin - AppStore</button></a>
 
 
