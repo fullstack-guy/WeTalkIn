1f43cc6b880b4cb4dd2cd705f59bd870e3dfe989
fullstack-guy <andriihryhoriev1@gmail.com>
2022-12-30 09:01:43 -0600

    added wt logo and wt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import wtlogo from './assets/logos/wt_logo.png';
 import './App.css';
 
 function App () {
     return (
         <div className="App">
-            hello
+
             <header className="App-header">
+
+                <img src={ wtlogo } className="App-logo" alt="logo" />
+
+                <h1>WeTalkin.com</h1>
                 <img src={ logo } className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Welcome to WeTalkin by Blossend!
                 </p>
                 <a
                     className="App-link"
-                    href="https://reactjs.org"
+                    href="https://blossend.com"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React Blossend
+                    Learn more about Blossend
                 </a>
             </header>
+
         </div>
     );
 }
