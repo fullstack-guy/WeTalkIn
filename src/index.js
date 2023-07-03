9fa3b8e2a265a06c05b1376bbf784db9f02e35e8
fullstack-guy <andriihryhoriev1@gmail.com>
2023-07-02 19:22:01 -0500

    update file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css';
-import reportWebVitals from './test/reportWebVitals';
+import reportWebVitals from './utils/test/reportWebVitals';
 
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
